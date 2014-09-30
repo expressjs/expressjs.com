40075ddb8742318de65e7cc4f669cd482929f6a0
Hage Yaapa <captain@hacksparrow.com>
2014-10-01 02:13:48 +0530

    better syntax highlight

--- app.js
+++ app.js
@@ -27,11 +27,15 @@ $(function(){
     }
   })
 
-  setTimeout(function () {
-    $('pre code.lang-js').each(function(){
-      $(this).html(highlight($(this).text()));
-    })
-  }, 1000)
+
+  $('code.lang-js').each(function(){
+    $(this).addClass('language-javascript').removeClass('lang-js')
+
+    //Prism.highlightElement(this)
+    //console.log(this)
+  })
+
+  Prism.highlightAll()
 
 })
 
@@ -74,16 +78,3 @@ $(function(){
     prev = a;
   })
 })
-
-// js highlighter
-function highlight(js) {
-  return js
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/\/\/(.*)/gm, '<span class="comment">//$1</span>')
-    .replace(/('.*?')/gm, '<span class="string">$1</span>')
-    .replace(/(\d+\.\d+)/gm, '<span class="number">$1</span>')
-    .replace(/(\d+)/gm, '<span class="number">$1</span>')
-    .replace(/\bnew *(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
-    .replace(/\b(function|new|throw|return|var|if|else)\b/gm, '<span class="keyword">$1</span>')
-}
