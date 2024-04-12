25807d19b65a3ceb2094334460c3526a584a8540
Chris Del <drumgod101@gmail.com>
2024-04-11 20:53:20 -0600

    Issue ##1382: work on JS and CSS

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,41 @@
 
 // theme toggle
-const darkModeToggle = document.querySelector('.theme-toggle');
-console.log(darkModeToggle);
+const themeToggleSwitch = document.querySelector('.theme-toggle');
+// console.log(themeToggleSwitch)
+// const currentTheme = themeToggleSwitch.classList.contains('darkmode-theme') ? 'DARK' : 'DEFAULT'
+(() =>{
+    localStorage.setItem("darkmode", "true")
+    const isDarkMode = localStorage.getItem("darkmode")
+    console.log(isDarkMode)
+    if (isDarkMode === "true") {
+        loadDarkMode()
+        // remove default theme class
+        // load dark mode
+    } else {
+        // do nothing
+    }
+})();
+themeToggleSwitch.addEventListener('click', toggleTheme)
+
+function loadDarkMode() {
+    document.body.classList.add('dark-mode-body', 'dark-mode')
+    const banner = document.querySelector('#blm-banner')
+    banner.classList.add('dark-mode')
+    const header = document.querySelector('header')
+    header.classList.add('dark-mode')
+    const logo = document.querySelector('#logo')
+    logo.classList.add('dark-mode')
+    const navbar= document.querySelector('#navbar')
+    navbar.classList.add('dark-mode')
+}
+function toggleTheme(e) {
+    const isDarkMode = localStorage.getItem("darkmode")
+    co
+    if (isDarkMode) {
+        // remove default theme class
+        // load dark mode
+    } else {
+        // do nothing
+    }
+
+}
