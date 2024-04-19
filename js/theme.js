493ebc015c5605a652613a57651f135c743d4ef9
Chris Del <drumgod101@gmail.com>
2024-04-18 21:54:29 -0600

    Issue #1382 - add all script tags - finish JS file - finish all CSS for pages

--- js/theme.js
+++ js/theme.js
@@ -1,48 +1,29 @@
 
-// theme toggle
-const themeToggleSwitch = document.querySelector('.theme-toggle');
-// console.log(themeToggleSwitch)
-// const currentTheme = themeToggleSwitch.classList.contains('darkmode-theme') ? 'DARK' : 'DEFAULT'
+// handle init page load & persist theme
 (() =>{
-    localStorage.setItem("darkmode", "true")
     const isDarkMode = localStorage.getItem("darkmode")
-    console.log(isDarkMode)
     if (isDarkMode === "true") {
         loadDarkMode()
-        // remove default theme class
-        // load dark mode
-    } else {
-        // do nothing
     }
 })();
-themeToggleSwitch.addEventListener('click', toggleTheme)
+document.querySelector('.theme-toggle').addEventListener('click', toggleTheme)
 
-function loadDarkMode() {
-    document.body.classList.add('dark-mode-body', 'dark-mode')
-    const banner = document.querySelector('#blm-banner')
-    banner.classList.add('dark-mode')
-    const header = document.querySelector('header')
-    header.classList.add('dark-mode')
-    const logoTop = document.querySelector('#logo')
-    logoTop.classList.add('dark-mode')
-    const logoMiddleContainer = document.querySelector('#description .express')
-    console.log(logoMiddleContainer)
-    logoMiddleContainer.classList.add('dark-mode')
-    const navbar= document.querySelector('#navbar')
-    navbar.classList.add('dark-mode')
-    const installCmd = document.querySelector('#install-command')
-    installCmd.classList.add('dark-mode')
-    const announcements = document.querySelector('#announcements')
-    announcements.classList.add('dark-mode')
-}
 function toggleTheme(e) {
     const isDarkMode = localStorage.getItem("darkmode")
-    co
-    if (isDarkMode) {
-        // remove default theme class
-        // load dark mode
+    // turn off
+    if (isDarkMode === "true") {
+        localStorage.setItem("darkmode", "false")
+        unloadDarkMode()
+    // turn on
     } else {
-        // do nothing
+        console.log('set to true?')
+        localStorage.setItem("darkmode", "true")
+        loadDarkMode()
     }
-
+}
+function loadDarkMode() {
+    document.body.classList.add('dark-mode')
+}
+function unloadDarkMode() {
+    document.body.classList.remove('dark-mode')
 }
