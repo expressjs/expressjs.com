e822d151d39be6350b77195c0d8cdd6d6ffad4e5
Chris Del <drumgod101@gmail.com>
2024-04-20 16:50:49 -0600

    issue #1382 - add check for local storage

--- js/theme.js
+++ js/theme.js
@@ -1,29 +1,31 @@
-
-// handle init page load & persist theme
-(() =>{
-    const isDarkMode = localStorage.getItem("darkmode")
-    if (isDarkMode === "true") {
-        loadDarkMode()
-    }
-})();
-document.querySelector('.theme-toggle').addEventListener('click', toggleTheme)
+// init page load & persist theme
+if (hasLocalStorage) {
+  const isDarkMode = localStorage.getItem('darkmode')
+  if (isDarkMode === 'true') darkModeOn()
+  document.querySelector('.theme-toggle').addEventListener('click', toggleTheme)
+} else {
+  // remove theme-toggle icon
+  document.querySelector('#theme-icon-container').remove()
+}
 
 function toggleTheme(e) {
-    const isDarkMode = localStorage.getItem("darkmode")
-    // turn off
-    if (isDarkMode === "true") {
-        localStorage.setItem("darkmode", "false")
-        unloadDarkMode()
+  const isDarkMode = localStorage.getItem('darkmode')
+  // turn off
+  if (isDarkMode === 'true') {
+    localStorage.setItem('darkmode', 'false')
+    darkModeOff()
     // turn on
-    } else {
-        console.log('set to true?')
-        localStorage.setItem("darkmode", "true")
-        loadDarkMode()
-    }
+  } else {
+    localStorage.setItem('darkmode', 'true')
+    darkModeOn()
+  }
 }
-function loadDarkMode() {
-    document.body.classList.add('dark-mode')
+function darkModeOn() {
+  document.body.classList.add('dark-mode')
 }
-function unloadDarkMode() {
-    document.body.classList.remove('dark-mode')
+function darkModeOff() {
+  document.body.classList.remove('dark-mode')
+}
+function hasLocalStorage() {
+  return typeof Storage !== 'undefined'
 }
