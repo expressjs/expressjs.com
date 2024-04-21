6c016b52663e4aac43efaa43af163bac34e7292d
Chris Del <drumgod101@gmail.com>
2024-04-21 17:27:15 -0600

    issue #1382 - remove error line css - add system style check - remove local storage use until toggled

--- js/theme.js
+++ js/theme.js
@@ -1,23 +1,49 @@
-// init page load & persist theme
-if (hasLocalStorage) {
-  const isDarkMode = localStorage.getItem('darkmode')
-  if (isDarkMode === 'true') darkModeOn()
-  document.querySelector('.theme-toggle').addEventListener('click', toggleTheme)
+// system theme check
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  if(!hasLocalStorage()) {
+    darkModeOn()
+    // remove icon - cannot turn off
+    document.querySelector('#theme-icon-container').remove()
+  } else {
+    // no use local storage til required
+    const isDarkMode = localStorage.getItem('darkmode')
+    // unless toggled off, darkmode is on
+    isDarkMode === 'false' ? darkModeOff()
+    : darkModeOn()
+    document.querySelector('.theme-toggle')
+    .addEventListener('click', toggleTheme)
+  }
 } else {
-  // remove theme-toggle icon
-  document.querySelector('#theme-icon-container').remove()
+  if (hasLocalStorage()) {
+    const isDarkMode = localStorage.getItem('darkmode')
+    if (isDarkMode === 'true') darkModeOn()
+    document
+      .querySelector('.theme-toggle')
+      .addEventListener('click', toggleTheme)
+  } else {
+    // remove theme-toggle icon
+    document.querySelector('#theme-icon-container').remove()
+  }
 }
 
 function toggleTheme(e) {
   const isDarkMode = localStorage.getItem('darkmode')
-  // turn off
   if (isDarkMode === 'true') {
     localStorage.setItem('darkmode', 'false')
     darkModeOff()
-    // turn on
-  } else {
+  } else if (isDarkMode === 'false') {
     localStorage.setItem('darkmode', 'true')
     darkModeOn()
+    // isDarkMode stilll undefined
+  } else {
+    // need to check state 
+    if(darkModeState()) {
+      localStorage.setItem('darkmode', 'false')
+      darkModeOff()
+    } else {
+      localStorage.setItem('darkmode', 'true')
+      darkModeOn()
+    }
   }
 }
 function darkModeOn() {
@@ -25,6 +51,9 @@ function darkModeOn() {
 }
 function darkModeOff() {
   document.body.classList.remove('dark-mode')
+}
+function darkModeState() {
+  return document.body.classList.contains('dark-mode')
 }
 function hasLocalStorage() {
   return typeof Storage !== 'undefined'
