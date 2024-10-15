be5705f0250e2d281d83a06547bc2041f7117332
Sebastian Beltran <bjohansebas@gmail.com>
2024-10-15 14:18:01 -0500

    improve script and catch error

--- get-express-version.mjs
+++ get-express-version.mjs
@@ -1,22 +1,20 @@
-import { writeFileSync } from 'node:fs'
-import { readFile } from 'node:fs/promises'
+import { readFile, writeFile } from 'node:fs/promises'
 import path from 'node:path'
 
 const NPMURL = 'https://registry.npmjs.org/express'
 
-const response = await fetch(NPMURL).then((res) => res.json())
+const response = await (await fetch(NPMURL)).json()
 
 const { next, latest } = response['dist-tags']
 
-const contentFile = await readFile(
-  path.resolve(path.join('_data', 'express.yml')),
-  'utf8'
-)
-  .then((content) =>
-    content.replace(/current_version: ".*"/, `current_version: "${latest}"`)
-  )
-  .then((content) =>
-    content.replace(/next_version: ".*"/, `next_version: "${next}"`)
-  )
+try {
+  const filePath = path.resolve(path.join('_data', 'express.yml'))
+  let content = await readFile(filePath, 'utf8')
 
-writeFileSync('_data/express.yml', contentFile)
+  content = content.replace(/current_version: ".*"/, `current_version: "${latest}"`)
+  content = content.replace(/next_version: ".*"/, `next_version: "${next}"`)
+
+  await writeFile(filePath, content, 'utf8')
+} catch (error) {
+  console.error('Error updating versions in _data/express.yml:', error)
+}
