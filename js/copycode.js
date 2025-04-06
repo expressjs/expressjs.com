04fdc30ffc1f4c3969b22d3784c2fe757bc34654
shubham <oulkarshubhu@gmail.com>
2025-04-07 02:34:42 +0530

    handle failed copy code

--- js/copycode.js
+++ js/copycode.js
@@ -19,20 +19,39 @@ codeBlocks.forEach((block) => {
 async function copyCode(block, button) {
     const code = block.querySelector("code");
     const text = code.innerText;
-  
-    await navigator.clipboard.writeText(text);
-    // screen reader will  announce text is copied to clipboard
-    button.setAttribute("aria-live", "polite");
-    button.setAttribute("aria-label","code is copied!"); 
-    button.classList.add("copied");
 
-    // remove previous timer on multiple clicks (data-timer-id)
-    if (button.dataset.timerId) clearTimeout(button.dataset.timerId);
-    
-    const timer = setTimeout(() => {
-      button.classList.remove("copied");
-      button.setAttribute("aria-label","click to copy code");
-    }, 1000);
+    try {
+      // success UI update
+      await navigator.clipboard.writeText(text);
+      // screen reader will  announce text is copied to clipboard
+      button.setAttribute("aria-live", "polite");
+      button.setAttribute("aria-label","code is copied!"); 
+      button.classList.add("copied");
 
-    button.dataset.timerId = timer;
+      // remove previous timer on multiple clicks (data-timer-id)
+      if (button.dataset.timerId) clearTimeout(button.dataset.timerId);
+      
+      const timer = setTimeout(() => {
+        button.classList.remove("copied");
+        button.setAttribute("aria-label","click to copy code");
+      }, 1000);
+
+      button.dataset.timerId = timer;
+    }catch{
+      //  failed UI update
+      // screen reader will  announce text is copied to clipboard
+      button.setAttribute("aria-live", "polite");
+      button.setAttribute("aria-label","failed!"); 
+      button.classList.add("failed");
+
+      // remove previous timer on multiple clicks (data-timer-id)
+      if (button.dataset.timerId) clearTimeout(button.dataset.timerId);
+      
+      const timer = setTimeout(() => {
+        button.classList.remove("failed");
+        button.setAttribute("aria-label","click to copy code");
+      }, 100000);
+
+      button.dataset.timerId = timer;
+    }
   };
