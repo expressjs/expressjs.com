9ce8ec4904b6b457bd3fa9143df79a6abd59d74c
Mohit5Upadhyay <upadhyaym7060@gmail.com>
2025-07-15 13:39:23 +0530

    apply reviewer changes: Update scroll-margin-top to 60px and remove preventDefault for TOC links

--- js/menu.js
+++ js/menu.js
@@ -183,7 +183,6 @@ toggleBtn?.addEventListener("click", (e) => {
 // Open/Close sub TOC content on click
 document.querySelectorAll("#menu > li > a").forEach((link) => {
 	link.addEventListener("click", function (event) {
-	  event.preventDefault(); // stop navigation to submenu
   
 	  // Find the closest parent <li>
 	  const closestLiParent = link.closest("li");
