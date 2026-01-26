3e2081a6e15b827ff373ff4d74a9376fa64bc68d
Francesca Giannino <francesca.giannino86@gmail.com>
2026-01-26 15:45:41 +0100

    chore: add vscode settings and icon package

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,8 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), icon()]
 });
