6b6ae94c06a8656815220420a1c0d186d9859911
Francesca Giannino <francesca.giannino86@gmail.com>
2026-01-30 13:35:01 +0100

    fix: style order by using @layer

--- astro/src/components/patterns/index.ts
+++ astro/src/components/patterns/index.ts
@@ -6,4 +6,5 @@
 
 export { default as Breadcrumbs } from './Breadcrumbs/Breadcrumbs.astro';
 export { default as Header } from './Header/Header.astro';
+export { default as Sidebar } from './Sidebar/Sidebar.astro';
 export { default as ThemeSwitcher } from './ThemeSwitcher/ThemeSwitcher.astro';
