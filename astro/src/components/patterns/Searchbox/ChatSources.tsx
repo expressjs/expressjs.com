e5f46f19aeafec86e24f3da2489c5397ddcb31da
Francesca Giannino <francesca.giannino86@gmail.com>
2026-03-19 11:46:42 +0100

    style: refine chat sources

---
+++ astro/src/components/patterns/Searchbox/ChatSources.tsx
@@ -0,0 +1,43 @@
+import { Icon } from '@iconify/react';
+import type { Interaction, AnyObject } from '@orama/core';
+import { ChatInteractions } from '@orama/ui/components';
+import type { FC } from 'react';
+import './ChatSources.css';
+
+type ChatSourcesProps = {
+  interaction: Interaction;
+};
+
+export const ChatSources: FC<ChatSourcesProps> = ({ interaction }) => {
+  if (!interaction?.sources) {
+    return null;
+  }
+
+  return (
+    <div className="chat-sources-scroll-container">
+      <ChatInteractions.Sources interaction={interaction} className="chat-sources" itemClassName="">
+        {(document: AnyObject, index: number) => (
+          <div className="chat-sources-item" key={index}>
+            {!!document.title && typeof document.title === 'string' && (
+              <a
+                href={document.url as string}
+                className="chat-sources-link"
+                data-focus-on-arrow-nav
+              >
+                <h3 className="chat-sources-title">
+                  <Icon icon="fluent:link-20-regular" width={16} height={16} />
+                  {(document.title as string).slice(0, 30) || ''}
+                  {(document.title as string).length > 30 ? '...' : ''}
+                </h3>
+                <p className="chat-sources-excerpt">
+                  {(document.content as string).slice(0, 60)}
+                  ...
+                </p>
+              </a>
+            )}
+          </div>
+        )}
+      </ChatInteractions.Sources>
+    </div>
+  );
+};
