3857ab576fd895d4773ddf3b4e77db2c54c957f0
Sebastian Beltran <bjohansebas@gmail.com>
2026-03-22 15:15:35 -0500

    feat: integrate sitemap functionality and add robots.txt (#2217)

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import expressiveCode from 'astro-expressive-code';
+
+// TODO: add redirecto for blog posts
 
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_PREVIEW_SITE = process.env.CONTEXT !== 'production' && process.env.DEPLOY_PRIME_URL;
 
 const site = NETLIFY_PREVIEW_SITE || 'https://expressjs.com';
-
-// TODO: add redirecto for blog posts
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,5 +27,22 @@ export default defineConfig({
     }),
     mdx(),
     icon(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'en',
+        locales: {
+          de: 'de',
+          en: 'en',
+          es: 'es',
+          fr: 'fr',
+          ja: 'ja',
+          it: 'it',
+          ko: 'ko',
+          'pt-br': 'pt-BR',
+          'zh-cn': 'zh-CN',
+          'zh-tw': 'zh-TW',
+        },
+      },
+    }),
   ],
 });
