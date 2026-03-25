e0faf05cb3515c2fd6dbfed7aec4198ac9e7a3fe
Francesca Giannino <francesca.giannino86@gmail.com>
2026-03-25 12:21:21 +0100

    feat: add PostHeader component

--- astro/src/components/patterns/index.ts
+++ astro/src/components/patterns/index.ts
@@ -19,3 +19,5 @@ export { default as CardList } from './CardList/CardList.astro';
 export { default as PostCard } from './PostCard/PostCard.astro';
 export { default as Pagination } from './Pagination/Pagination.astro';
 export { default as PageHead } from './PageHead/PageHead.astro';
+export { default as PageTopbar } from './PageTopbar/PageTopbar.astro';
+export { default as PostHeader } from './PostHeader/PostHeader.astro';
